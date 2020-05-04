9dc023524111496858883c19f2c5f91f89c3e9bf
mahesh2020repo <mahesh.rajput@atos.net>
2020-05-04 06:42:13 +0530

    First Commit

---
+++ ncalculator/src/test/java/com/mahesh/NewTest.java
@@ -0,0 +1,10 @@
+package com.mahesh;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  System.out.println("---------------------**** Test NG test case executed...");
+  }
+}
